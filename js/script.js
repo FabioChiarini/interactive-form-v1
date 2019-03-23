e2da4d775c31d4cbb2c78e48cfe5fb329cb5a2ce
Fabio Chiarini <chiarrss@gmail.com>
2019-03-23 16:35:28 +0100

    Set focus on first item

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 Treehouse Techdegree:
 FSJS project 3 - Interactive form
 ******************************************/
+
+// set focus on the first text field
+$("#name").focus();
+$('#other-title').hide();
+
+
+
+/*
+”T-Shirt Info” section
+For the T-Shirt "Color" menu, only display the color options that match the design selected in the "Design" menu.
+If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
+If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
+When a new theme is selected from the "Design" menu, the "Color" field and drop down menu is updated.*/
