804ddbfd0efc2afa5f703d1b9225d84db8855efa
Fabio Chiarini <chiarrss@gmail.com>
2019-03-24 22:16:30 +0100

    Display/hide colors based on shirt design
    Function is working now. Still to refine though.

--- js/script.js
+++ js/script.js
@@ -5,13 +5,21 @@ FSJS project 3 - Interactive form
 
 // set focus on the first text field
 $("#name").focus();
+//hide the text field for the other-title jobs
 $('#other-title').hide();
 
+//listen for user selection of shirt design and hide/display colors accordingly
+$('#design').change(function() {
+  //get the value of the shirt selected by the user
+  let selectedShirt = $(this).children("option:selected").val();
+  //display/hide the respective colors
+  if (selectedShirt === 'js puns') {
+      $('#colors-js-puns option:eq(0), #colors-js-puns option:eq(1), #colors-js-puns option:eq(2)').show();
+      $('#colors-js-puns option:eq(3), #colors-js-puns option:eq(4), #colors-js-puns option:eq(5)').hide();
 
+    } else if (selectedShirt === 'heart js') {
+      $('#colors-js-puns option:eq(3), #colors-js-puns option:eq(4), #colors-js-puns option:eq(5)').show();
+      $('#colors-js-puns option:eq(0), #colors-js-puns option:eq(1), #colors-js-puns option:eq(2)').hide();
+    }
 
-/*
-”T-Shirt Info” section
-For the T-Shirt "Color" menu, only display the color options that match the design selected in the "Design" menu.
-If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
-If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
-When a new theme is selected from the "Design" menu, the "Color" field and drop down menu is updated.*/
+});
