2929a85dc21fd085eb4ecf84d7070f54d0a240c2
Fabio Chiarini <chiarrss@gmail.com>
2019-03-24 22:41:29 +0100

    Fixed showing first option in dropdown menu
    Fixed this issue. Wonder what i need to do with the "Select theme" choice.

--- js/script.js
+++ js/script.js
@@ -8,18 +8,29 @@ $("#name").focus();
 //hide the text field for the other-title jobs
 $('#other-title').hide();
 
+
 //listen for user selection of shirt design and hide/display colors accordingly
 $('#design').change(function() {
   //get the value of the shirt selected by the user
   let selectedShirt = $(this).children("option:selected").val();
   //display/hide the respective colors
   if (selectedShirt === 'js puns') {
+      //hide and show the colors for the respective design
       $('#colors-js-puns option:eq(0), #colors-js-puns option:eq(1), #colors-js-puns option:eq(2)').show();
       $('#colors-js-puns option:eq(3), #colors-js-puns option:eq(4), #colors-js-puns option:eq(5)').hide();
+      //auto select the first color
+      $("#color option[selected]").attr('selected', false);
+      $("#color option[value='cornflowerblue']").attr('selected', true);
 
     } else if (selectedShirt === 'heart js') {
+      //hide and show the colors for the respective design
       $('#colors-js-puns option:eq(3), #colors-js-puns option:eq(4), #colors-js-puns option:eq(5)').show();
       $('#colors-js-puns option:eq(0), #colors-js-puns option:eq(1), #colors-js-puns option:eq(2)').hide();
+      //auto select the first color
+      $("#color option[selected]").attr('selected', false);
+      $("#color option[value='steelblue']").attr('selected', true);
     }
+
+
 
 });
