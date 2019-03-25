6549fb560482f58439ad8082940ccf827222e5cf
Fabio Chiarini <chiarrss@gmail.com>
2019-03-25 05:49:50 +0100

    Get checked/unchecked checkboxes
    Get if a checkbox is being checked or unchecked

--- js/script.js
+++ js/script.js
@@ -30,7 +30,20 @@ $('#design').change(function() {
       $("#color option[selected]").attr('selected', false);
       $("#color option[value='steelblue']").attr('selected', true);
     }
+});
 
+
+//get if a checkbox is checked/unchecked
+$('.activities input').click(function(){
+  $(this).attr('checked', true);
+  console.log($(this).is(":checked"));
 
 
 });
+/*Some events are at the same day and time as others.
+If the user selects a workshop, don't allow selection of a workshop at the same day and time --
+you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
+When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
+As a user selects activities, a running total should display below the list of checkboxes.
+For example, if the user selects "Main Conference", then Total: $200 should appear.
+If they add 1 workshop, the total should change to Total: $300.*/
